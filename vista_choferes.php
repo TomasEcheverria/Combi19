22d66d9d9b9278f0b4983a5e2481123dafcccf0a
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-05-06 17:34:11 -0400

    archivos movidos y path modificados

--- vista_choferes.php
+++ vista_choferes.php
@@ -1,7 +1,7 @@
 <?php
-    include '../includes/dbh.php';
-    include '../includes/choferes.php';
-    include '../includes/query_choferes.php';
+    include 'php/dbh.php';
+    include 'php/choferes.php';
+    include 'php/query_choferes.php';
 ?>
 
 <!doctype html>
@@ -26,7 +26,7 @@
     }
     ?>
 </ul>
-<form action ="../includes/alta_chofer.php" class="row g-3" method ="POST">
+<form action ="php/alta_chofer.php" class="row g-3" method ="POST">
   <div class="col-md-6">
     <label for="inputEmail4" class="form-label">Nombre</label>
     <input type="text" class="form-control" name="firstName" placeholder="" value="" required="">
