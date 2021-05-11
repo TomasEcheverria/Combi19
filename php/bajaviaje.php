1973a00597237eae6d1d82a670ed38ccdb2ace12
TomasEcheverria <tomiecheverria00@gmail.com>
2021-05-11 20:32:05 -0300

    arreglo de listar

--- php/bajaviaje.php
+++ php/bajaviaje.php
@@ -3,32 +3,30 @@
 	$link = conectar();
     include "classLogin.php"; 
 	if(isset($_POST['borrar'])){
-		$id=$_POST['id'];
-		$query10="DELETE FROM me_gusta WHERE mensaje_id='$id'";
+		$idv=$_POST['idv'];
+		$query10="UPDATE viajes SET activo='0' WHERE idv='$idv'";
 		$result10= mysqli_query ($link, $query10) or die ('Consulta 10 fallida ' .mysqli_error($link));
-		 $consulta1="DELETE FROM mensaje WHERE id='$_POST[id]'";
-		 $resultado1 = mysqli_query ($link, $consulta1) or die ('Consulta 1 query fallida: ' .mysqli_error($link));
-		 if($resultado1){
+		 if($result10){
 		 	 $exito=true;
 		               }
 	} 
  ?>
 <html>
 <head> 
-	<title> Mensajes </title>
+	<title> Combi 10 </title>
 	<link rel="stylesheet" type="text/css" href= " ../css/Estilos.css" media="all" > 
 </head>
 <body class = "body" >
 	<div class="div_body">
 			<div class="div_superior" >
-				<p> The Wall <img src="../css/images/muro.jpg" class="div_icono">
+				<p>  Combi 19 <img src="../css/images/muro.jpg" class="div_icono">
 				</p>
 			</div>
 	<br> <br>
 	<?php 
 	if ((isset($exito)) and (($exito==true))){
 	?>
-	Mensaje Borrado <br><br>
+	Viaje Borrado exitosamente <br><br>
 	<a href="../pagprincipal.php"> Click aqui para volver  &nbsp;&nbsp;&nbsp; </a>
 	<?php
 	} 
