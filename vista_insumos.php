d16a0d89abc53d85871b26ffa4477103821e3f24
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-05-12 03:31:30 -0400

    cambios diseño vista_insumos.php

--- vista_insumos.php
+++ vista_insumos.php
@@ -31,11 +31,13 @@
     <title>Vista de Insumos</title>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href= "css/Estilos.css" media="all" >
+        <link rel="stylesheet" type="text/css" href= "css/bootstrap.min.css" media="all" >
 </head>
 <?php try{ 
     $usuario -> administrador($tipo);
     ?>
-<body>
+<body class="body">
 
     <div class="card">
     <div class="card-header text-center">
