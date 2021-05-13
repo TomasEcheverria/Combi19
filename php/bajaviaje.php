996085f736bb63e1b782a91aa3e9f0eee722c27d
TomasEcheverria <tomiecheverria00@gmail.com>
2021-05-13 07:26:52 -0300

    borrado viajes

--- php/bajaviaje.php
+++ php/bajaviaje.php
@@ -4,12 +4,21 @@
     include "classLogin.php"; 
 	if(isset($_POST['borrar'])){
 		$idv=$_POST['idv'];
+		$exito=false;
+		$query50 ="SELECT * FROM viajes  WHERE idv='$idv'";
+        $result50=mysqli_query ($link, $query50) or die ('Consulta query50 fallida: ' .mysqli_error($link));
+        $datos=(mysqli_fetch_array($result50)); 
+
+		if($datos['estado'] != 'en curso'){
 		$query10="UPDATE viajes SET activo='0' WHERE idv='$idv'";
 		$result10= mysqli_query ($link, $query10) or die ('Consulta 10 fallida ' .mysqli_error($link));
 		 if($result10){
 		 	 $exito=true;
 		               }
+		}
+					
 	} 
+
  ?>
 <html>
 <head> 
@@ -26,7 +35,11 @@
 		Viaje Borrado exitosamente <br><br>
 		<a href="../listarviajes.php"> Click aqui para volver  &nbsp;&nbsp;&nbsp; </a>
 		<?php
-		} 
+		} else { ?>
+		 El viaje esta marcado como pendiente<br><br>
+		 <a href="../listarviajes.php"> Click aqui para volver  &nbsp;&nbsp;&nbsp; </a>
+		<?php
+		}
 		?>
 	</div>
 </body>
