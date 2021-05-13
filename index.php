19bdfa0a496fe09d803a41b68ac8ecae28b9d5d6
Qhiden <teoelizondo01@gmail.com>
2021-05-12 21:24:41 -0300

    index fachero

--- index.php
+++ index.php
@@ -10,70 +10,109 @@
 		<title>
 			Combi 19
 		</title>
-		<link rel="stylesheet" type="text/css" href= "css/Estilos.css" media="all" > 
+
+		<link rel="stylesheet" type="text/css" href= "css/bootstrap.min.css" media="all" > 
 		<script  src= "js/Passwordcheckbox.js"></script>
 		<script type="text/javascript" src="js/confirmarCerrarSesion.js"></script>
 	</head>
-		</div>
-	<body class = "body" >
-		<div class="div_body">
-			<div class="div_superior" >
-				<p> Combi 19 <img src="css/images/muro.jpg" class="div_icono">
-				</p>
+
+
+
+	<body style="margin: 1%" >
+		<div class="mx-auto">
+			<a  href="pagprincipal.php" >  
+				<div class ="text-center"><img src="css/images/logo_is.png" style="max-width: 15rem;"></div>	
+			</a>
+		</div> <br> 
+		
+
+		<?php
+			try {
+				$usuario -> noIniciada($usuarioID);	
+				$name='';
+				$password='';
+				if (isset($_GET['mensaje'])){
+					$name=$_SESSION['nombrei'];// en nombrei pongamos el email que es la clave
+					$password=$_SESSION['conti'];// aca la contraseña
+				}
+		?>
+
+
+
+		<div class="mx-auto" style="max-width: 30rem;">
+
+			<div class="card border-primary mb-3" style="max-width: 30rem;">	
+				<form action="php/iniciarsesion.php" name="iniciarsesion" method="post"> 			         
+
+					<div class="card-header">
+						<h2 class="text-center">Iniciar Sesión</h2>
+					</div>
+
+					<div class="card-body">
+
+      					<label for="exampleInputEmail1" class="form-label mt-4">Dirección de Email</label>
+      						<input type="email" class="form-control" name="nombre" id="nombre" value="<?php echo $name ?>" aria-describedby="emailHelp" placeholder="Email" required>
+      					<small id="emailHelp" class="form-text text-muted">No compartiremos tu email con terceros.</small>
+						<br>
+
+
+      					<label for="exampleInputPassword1" class="form-label mt-4">Contraseña</label>
+      						<input type="password" class="form-control"  name="cont" id="contra" value="<?php echo $password ?>" minlength="6" placeholder="Contraseña" required>
+						<br>
+
+      				<div class="form-check form-switch">
+        				<input class="form-check-input" type="checkbox" onclick="indexpassword()" id="flexSwitchCheckDefault">
+        				<label class="form-check-label" for="flexSwitchCheckDefault">Mostrar Contraseña</label>
+      				</div> 
+					<br>
+					<div class="text-center">
+						<input type="submit" value="Iniciar Sesion"  class="btn btn-primary"> <br> <br>
+						<a href="Registrarse.php" class="button">Registrarse como nuevo usuario </a> <br> <br>
+						<a href="pagprincipal.php" class="button"> Ingresar como invitado </a>
+						<br>
+					</div>
+					</div>
+				</form>			
 			</div>
-	<?php
-	try {
-		$usuario -> noIniciada($usuarioID);	
-		$name='';
-		$password='';
-		if (isset($_GET['mensaje'])){
-			$name=$_SESSION['nombrei'];// en nombrei pongamos el email que es la clave
-			$password=$_SESSION['conti'];// aca la contraseña
-		}
-	?>
-		<div class="div_registro">
-		<div class="inicio_sesion">
-			<form action="php/iniciarsesion.php" name="iniciarsesion" method="post" >
-				<h2> Inciar Sesi&oacuten </h2>   
-			         
-				<input type="text" name="nombre" size=30 id="nombre" value="<?php echo $name ?>" placeholder="Nombre Usuario" ><br><br></p>
-			          
-				<input type="password" name="cont" size=30  id="contra"	value="<?php echo $password ?>" minlength="6" placeholder="Clave" ><br>
-				<input type="checkbox" onclick="indexpassword()">Mostrar Contraseña<br></p>
-				<input type="submit" value="Iniciar Sesion"  class="btn"><br><br>
-				<a href="Registrarse.php" class="button">Registrarse como nuevo usuario </a><br><br>
-				<br>
-			</form>			
+
+
+			
+			<br>
+			<br>
 		</div>
-		<br>
-		<a href="pagprincipal.php" class="button"> Proceder a pagina principal como usuario invitado </a>
-	</div>
-	<?php
-			if (isset($_GET['mensaje'])){
-			?>
-			<div class="div_registro">
-		<?php echo ($_GET['mensaje'] . "<br><br>");
+
+
+		<div class="mx-auto" style="max-width: 35rem;">		
+		<?php	if (isset($_GET['mensaje'])){ ?>		
+			<div class="alert alert-dismissible alert-warning">
+			<?php echo ($_GET['mensaje'] . "<br><br>");
 				echo ("Por favor intente nuevamente");	
-			} ?>
+		} ?>
+			</div>
 		</div>
-		<div class= "div_foot">
-		<p> Made by :Grupo 40 </p>
-	</div> 
-	</div>
+
+
 	</body>
-	
-	<?php
-	} catch (Exception $e){
-			echo $e->getMessage();
-	?>
-		como <?php echo $_SESSION['email'] ?> <br><br>		
-			<a href="pagprincipal.php" > Click aqui para volver a la pagina principal </a><br><br>	
-			<a href="php/cerrarSesion.php" onclick="return SubmitForm(this.form)" value="Eliminar"> Click aqui para cerrar Sesion </a>
-		<div class= "div_foot">
-		<p> Made by :Grupo 40 </p>
+
+
+		<?php
+		} catch (Exception $e){
+			$msg= $e->getMessage();
+		?>
+		
+	<div class="mx-auto" style="max-width: 35rem;">
+		<div class="alert alert-dismissible alert-info">	
+		
+				<?php echo $msg. " como ". $_SESSION['email'] ?> <br><br>					
+				<a href="pagprincipal.php" > Volver a la pagina principal </a><br><br>	
+				<a href="php/cerrarSesion.php" onclick="return SubmitForm(this.form)" value="Eliminar"> Cerrar Sesion </a>
+		</div>
 	</div>
-		<?php	
-	}
-	?> 
+	<?php } ?> 
+	<div class="div-foot">
+			<figcaption class="blockquote-footer">
+				<cite title="Source Title">Made by : Grupo 40 </cite>
+			</figcaption>
+	</div>
 </html>
         
