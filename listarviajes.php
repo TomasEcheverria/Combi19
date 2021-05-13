ce308a386f3bb3a5cf01bb79bca2e463f0f7955c
Qhiden <teoelizondo01@gmail.com>
2021-05-12 22:50:02 -0300

    backup

--- listarviajes.php
+++ listarviajes.php
@@ -60,48 +60,28 @@
        						 $chofer=(mysqli_fetch_array($result51)); 
 		
        						 $query52 ="SELECT * FROM rutas WHERE idr='$idr'";
-<<<<<<< HEAD
         					$result52=mysqli_query ($link, $query52) or die ('Consulta query51 fallida: ' .mysqli_error($link));
-=======
-        					$result52=mysqli_query ($link, $query52) or die ('Consulta query52 fallida: ' .mysqli_error($link));
->>>>>>> 2ea146212c17a5fe37a4b034aa098aba29f54408
       						  $rutas=(mysqli_fetch_array($result52));
 							
 							
 								$id= $chofer['id'];
 								$query53 ="SELECT * FROM combis WHERE idu='$id'";
-<<<<<<< HEAD
 								$result53=mysqli_query ($link, $query53) or die ('Consulta query51 fallida: ' .mysqli_error($link));
-=======
-								$result53=mysqli_query ($link, $query53) or die ('Consulta query53 fallida: ' .mysqli_error($link));
->>>>>>> 2ea146212c17a5fe37a4b034aa098aba29f54408
       						  	$combi=(mysqli_fetch_array($result53));
 								$asientos= $combi['cantidad_asientos'];
 
 								$query54="SELECT * FROM pasajes WHERE idv='$idv'";
-<<<<<<< HEAD
 								$result54=mysqli_query ($link, $query54) or die ('Consulta query51 fallida: ' .mysqli_error($link));
-=======
-								$result54=mysqli_query ($link, $query54) or die ('Consulta query54 fallida: ' .mysqli_error($link));
->>>>>>> 2ea146212c17a5fe37a4b034aa098aba29f54408
       						  	$pasajes=(mysqli_num_rows($result54));
 
 								$cantidad = $asientos - $pasajes;
 
 								$query58="SELECT * FROM lugar WHERE idl='$rutas[codigo_postal_origen]'";
-<<<<<<< HEAD
 								$result58=mysqli_query ($link, $query58) or die ('Consulta query51 fallida: ' .mysqli_error($link));
       						  	$origen=(mysqli_fetch_array($result58));
 								
 								$query59="SELECT * FROM lugar WHERE idl='$rutas[codigo_postal_detino]'";
 								$result59=mysqli_query ($link, $query59) or die ('Consulta query51 fallida: ' .mysqli_error($link));
-=======
-								$result58=mysqli_query ($link, $query58) or die ('Consulta query55 fallida: ' .mysqli_error($link));
-      						  	$origen=(mysqli_fetch_array($result58));
-								
-								$query59="SELECT * FROM lugar WHERE idl='$rutas[codigo_postal_detino]'";
-								$result59=mysqli_query ($link, $query59) or die ('Consulta query56 fallida: ' .mysqli_error($link));
->>>>>>> 2ea146212c17a5fe37a4b034aa098aba29f54408
 								$destino=(mysqli_fetch_array($result59));
 
 						if(($chofer['activo'] == 1) and ($rutas['activo']  == 1 ) and ($viajes['activo']== 1 )){	
