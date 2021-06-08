cee81c01082e4671fdb9ece88c8f1a1bf10d9c61
TomasEcheverria <tomiecheverria00@gmail.com>
2021-06-08 02:24:09 -0300

    pagina principal , listado de viaje y borrado de viajes

--- php/bajaviaje.php
+++ php/bajaviaje.php
@@ -2,30 +2,42 @@
   include "../BD.php";// conectar y seleccionar la base de datos
 	$link = conectar();
     include "classLogin.php"; 
-	if(isset($_POST['borrar'])){
+?>
+<html>
+<head> 
+	<title> Combi 10 </title>
+	<link rel="stylesheet" type="text/css" href= " ../css/bootstrap.min.css" media="all" > 
+</head>
+</body> 
+<?php
+	//if(isset($_POST['borrar'])){
 		$idv=$_POST['idv'];
+		$total=$_POST['total'];
 		$exito=false;
+		$marcadas=true;
 		$query50 ="SELECT * FROM viajes  WHERE idv='$idv'";
         $result50=mysqli_query ($link, $query50) or die ('Consulta query50 fallida: ' .mysqli_error($link));
         $datos=(mysqli_fetch_array($result50)); 
-
-		if($datos['estado'] != 'curso'){
+		for ($i = 1; $i <= $total; $i++) {
+			if(!isset($_POST[$i])){
+				$marcadas=false;
+			}
+		}
+		if($marcadas){
+		if($datos['estado'] != 'en curso'){
 		$query10="UPDATE viajes SET activo='0' WHERE idv='$idv'";
 		$result10= mysqli_query ($link, $query10) or die ('Consulta 10 fallida ' .mysqli_error($link));
 		 if($result10){
 		 	 $exito=true;
 		               }
+		
 		}
-					
-	} 
+		}			
+	//} 
 
  ?>
-<html>
-<head> 
-	<title> Combi 10 </title>
-	<link rel="stylesheet" type="text/css" href= "css/bootstrap.min.css" media="all" > 
-</head>
-<body>
+
+
 	<div class="text-center">
 			
 		<br> <br>
@@ -36,7 +48,9 @@
 		<a href="../listarviajes.php"> Click aqui para volver  &nbsp;&nbsp;&nbsp; </a>
 		<?php
 		} else { ?>
-		 El viaje esta marcado como pendiente<br><br>
+		 El viaje esta marcado como en curso <br><br>
+		 <?php if (!$marcadas) {?> 
+		No se marcaron todas las checkbox <?php echo $total; }?></br>
 		 <a href="../listarviajes.php"> Click aqui para volver  &nbsp;&nbsp;&nbsp; </a>
 		<?php
 		}
