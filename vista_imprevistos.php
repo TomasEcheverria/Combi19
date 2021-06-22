b5849faab02b2535c268a6b7a18ddfdfd74c53bf
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-06-22 15:17:09 -0400

    solo se muestran imprevistos del chofer logeado

--- vista_imprevistos.php
+++ vista_imprevistos.php
@@ -5,16 +5,18 @@
     include 'php/classLogin.php';
     $usuario= new usuario();
     $usuario -> tipoUsuario($tipo);
+    $usuario -> session ($usuarioID);
+    $usuario -> id($id);
+    $idChofer = $id;
 
-
-    function getViajes(){
+    function getViajes($idChofer){
         $db = conectar();
         $sql = "SELECT v.idv, l1.nombre as origen, l2.nombre as destino, r.descripcion,v.fecha, v.hora, v.imprevisto FROM `viajes` v
         INNER JOIN usuarios u ON u.id = v.idc
         INNER  JOIN rutas r ON r.idr = v.idr
         INNER JOIN lugares l1 ON r.codigo_postal_origen = l1.idl
         INNER JOIN lugares l2 ON r.codigo_postal_destino = l2.idl
-        WHERE v.estado <> 'pendiente'";
+        WHERE v.estado <> 'pendiente' AND v.idc = $idChofer";
         $result = mysqli_query($db,$sql);
         $numRows = $result->num_rows;
         if ($numRows > 0) {
@@ -33,14 +35,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vista de Insumos</title>
+    <title>Vista de Imprevistos</title>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href= "css/Estilos.css" media="all" >
         <link rel="stylesheet" type="text/css" href= "css/bootstrap.min.css" media="all" >
 </head>
 <?php try{ 
-    $usuario -> administrador($tipo);
+    $usuario-> iniciada($usuarioID);
     ?>
 <body>
 
@@ -54,7 +56,7 @@
 
             <?php
               // Tabla de insumos
-              $viajes = getViajes();
+              $viajes = getViajes($idChofer);
               if(!empty($viajes)){ // Esto seguramente deberia ser una excepcion
                   // Se chequea que existan datos para mostrar
                   $tabla = "
@@ -73,7 +75,7 @@
                   ";
                   $datos_tabla = "";
                 foreach ($viajes as $value) {
-                    $id = $value['idv'];
+                    $id_viaje = $value['idv'];
                     $imprevisto = $value['imprevisto'];
                     $datos_tabla = $datos_tabla . 
                     "<tr id='tr_$id'>".
@@ -83,14 +85,14 @@
                       "<td>". $value['fecha'] . "</td>".
                       "<td>". $value['hora'] . "</td>".
                       "<td>".                    
-                        " <button class='btn btn-primary' type='button' data-bs-toggle='collapse' data-bs-target='#collapseExample$id' aria-expanded='false' aria-controls='collapseExample'>
+                        " <button class='btn btn-primary' type='button' data-bs-toggle='collapse' data-bs-target='#collapseExample$id_viaje' aria-expanded='false' aria-controls='collapseExample'>
                             Ver Imprevisto
                         </button>".
                       "</td>".
                     "</tr>".
                     "<tr>".
                         "<td colspan='6' class='hiddenRow'>".
-                            "<div class='collapse' id='collapseExample$id'>".
+                            "<div class='collapse' id='collapseExample$id_viaje'>".
                                 "<h5>
                                 $imprevisto
                                 </h5>".
